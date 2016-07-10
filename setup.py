92ea3a8834fdf2a07da0cea62b29d414fbd94121
Nicholas Wolf <nicwolf1@gmail.com>
2016-07-09 19:18:16 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Distutils',
+	  version='0.1',
+	  description='PyGloss',
+	  author='Nicholas Wolf',
+	  url='https://www.nicwolf.me',
+	  packages=['pygloss'],
+	 )
